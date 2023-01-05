7b0020377de18f86d19b6ce784b86c3edf7b1cab
LukeOlende <olendeluke2k@gmail.com>
2023-01-05 16:33:29 +0300

    Dashboard Third Commit

--- dashboard/script.js
+++ dashboard/script.js
@@ -6,7 +6,7 @@ new Chart(ctx, {
       labels: ['Jan 22', 'Feb 22', 'Mar 22', 'Apr 22', 'May 22', 'Jun 22', 
         'Jul 22', 'Aug 22', 'Sep 22', 'Oct 22', 'Nov 22', 'Dec 22'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Per Month Usage',
         data: [10, 14, 22, 12, 9.6, 8, 
             9.8, 16, 16, 24, 24, 26],
         borderWidth: 1
@@ -15,9 +15,11 @@ new Chart(ctx, {
     options: {
       scales: {
         y: {
-          beginAtZero: true
+          beginAtZero: true,
+          ticks: {min: 0, max: 30}
         }
-      }
+      },
+      legend: {display: false}
     }
   });
 
